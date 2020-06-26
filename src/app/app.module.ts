e56ec190bff95dba2c20b1ea6497d243c1840145
Nazarii17 <nazar.tkachuk.28@gmail.com>
2020-06-27 01:31:31 +0300

    Configured RouterModule;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import {NavigationComponent} from './navigation/navigation.component';
 import {FeedbackComponent} from './feedback/feedback.component';
 import {NotesComponent} from './notes/notes.component';
 import {NotFoundComponent} from './not-found/not-found.component';
-import {Router, Routes} from "@angular/router";
+import {Router, RouterModule, Routes} from "@angular/router";
 
 const appRoutes: Routes = [
   {
@@ -39,6 +39,7 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     AppRoutingModule
   ],
   providers: [],
