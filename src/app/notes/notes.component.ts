601e9a984090fb75e86f55c68f48d2d05f4f3f67
Nazarii17 <nazar.tkachuk.28@gmail.com>
2020-06-27 20:12:25 +0300

    Added getAllNotebooks();

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Notebook} from "./model/notebook";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-notes',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notes.component.css']
 })
 export class NotesComponent implements OnInit {
+  notebooks: Notebook[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private http: HttpClient) {
   }
 
+  ngOnInit(): void {
+    this.getAllNotebooks();
+  }
+
+  public getAllNotebooks() {
+    let url = "http://localhost:8082/api/notebooks/all";
+    this.http.get<Notebook[]>(url).subscribe(
+      response => {
+        this.notebooks = response;
+      },
+      error => {
+        alert("An error has occurred")
+      }
+    );
+  }
 }
